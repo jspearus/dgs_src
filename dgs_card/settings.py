7f8254e4b411d94df6c4f418c5084f596993e2b4
jspearus <jspearus@gmail.com>
2022-05-12 18:38:48 -0500

    added files for production

--- dgs_card/settings.py
+++ dgs_card/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'grkb2t18&mt%aqe+5a(74&x*y=@*oi32^4v*yy$@f(3snd3)ta'
 SECRET_KEY = os.environ.get(
-    'grkb2t18&mt%aqe+5a(74&x*y=@*oi32^4v*yy$@f(3snd3)ta')
+    'DJANGO_SECRET_KEY', 'grkb2t18&mt%aqe+5a(74&x*y=@*oi32^4v*yy$@f(3snd3)ta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,4 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
