f343aae516420d7d4398c4c5876925da70809c1f
jspearus <jspearus@gmail.com>
2022-05-12 19:19:13 -0500

    update allowed hosts

--- dgs_card/settings.py
+++ dgs_card/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
