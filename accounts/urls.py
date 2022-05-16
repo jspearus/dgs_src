c443e7486323a6099e9405a6461ebb44a0535f0f
jspearus <35698087+jspearus@users.noreply.github.com>
2022-05-16 09:57:50 -0500

    updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 # accounts/urls.py
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, Register
+
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    #  path("registration/signup/", Register, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
 # accounts/views.py
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
+
+from dgs_card.views import home_page
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+def Register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            messages.info(request, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return HttpResponseRedirect("/") 
