4911810550cb95068d63fd4af02b45c8d55436c2
jspearus <jspearus@gmail.com>
2022-06-03 18:32:34 -0500

    updates

--- play_game/admin.py
+++ play_game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 
-from .models import GameCreator, CurrentGame
+from .models import GameCreator, CurrentGame, GameSave
 # Register your models here.
 admin.site.register(GameCreator)
 admin.site.register(CurrentGame)
+admin.site.register(GameSave)
--- play_game/models.py
+++ play_game/models.py
@@ -40,3 +40,25 @@ class CurrentGame(models.Model):
 
     def __str__(self):
         return self.game + str(self.cur_hole)
+
+
+class GameSave(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             default=1, null=True,
+                             on_delete=models.SET_NULL)
+    card = models.CharField(
+        max_length=140, blank=False, null=False)
+    park = models.CharField(
+        max_length=140, blank=False, null=False)
+    hole = models.IntegerField(blank=False, null=False)
+    holeNumber = models.IntegerField(blank=False, null=False)
+    holeSub = models.CharField(max_length=1, blank=True)
+    basket = models.CharField(max_length=10, blank=True)
+    tee = models.CharField(max_length=10)
+    distance = models.IntegerField(blank=False, null=False)
+    throws = models.IntegerField(blank=False, null=False)
+    par = models.IntegerField(blank=False, null=False)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.user) + self.card + str(self.timestamp)
