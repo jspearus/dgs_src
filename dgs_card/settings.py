9efe7a15e0e92953a6cb065e42e9e1bebd3ce5d6
jspearus <35698087+jspearus@users.noreply.github.com>
2022-06-11 17:35:39 -0500

    update

--- dgs_card/settings.py
+++ dgs_card/settings.py
@@ -23,10 +23,9 @@ SECRET=os.environ.get('SECRET_KEY')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get(
-    # 'DJANGO_SECRET_KEY', 'grkb2t18&mt%aqe+5a(74&x*y=@*oi32^4v*yy$@f(3snd3)ta')
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', SECRET)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
